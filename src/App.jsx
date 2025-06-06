0a65e52709dcb9020196d61aee4607dae3c3a595
Brenda Amaya <brendaamaya9@gmail.com>
2025-06-05 23:23:11 -0300

    exportación sigue

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,8 @@ function App() {
     const [categories, setCategories] = useState([]);
     const [sortField, setSortField] = useState("price");
     const [sortOrder, setSortOrder] = useState("asc");
-    const [page, setPage] = useState(1);
-    const [format, setFormat] = useState("");
+    const [page, setPage] = useState(1); //ponemos 1 porque empieza en la pag 1
+    const [format, setFormat] = useState(""); //selección del formato para la exportación
 
     const limit = 30;
 
@@ -124,27 +124,39 @@ function App() {
         containerRef.current.classList.toggle("dark-mode");
     };
 
-    //----------------------------Descarga de datos -----------------------------
+    //----------------------------------- Exportación de datos -------------------------------------
+    //Usamos blob que va a representar datos binarios y usamos la url para descargar ese blob
     const handleExport = () => {
-        const blob = new Blob([JSON.stringify(filteredProducts, null, 2)], {
+      if (format === "json") {
+        const blob = new Blob([JSON.stringify(filteredProducts, null, 2)], { //convertimos en string 
             type: "application/json",
         });
         const url = URL.createObjectURL(blob);
         triggerDownload(url, "productos.json");
+      } else if (format === "csv") {
+          const csvData = convertProductsToCsvData(filteredProducts); //usamos la función de abajo con los productos filtrados
+          const csvContent = csvData.map(row => row.join(",")).join("\n"); //toma cada fila y convierte sus valores en una línea de texto separada por comas
+          const blob = new Blob([csvContent], { type: "text/csv" });
+          const url = URL.createObjectURL(blob);
+          triggerDownload(url, "productos.csv");
+        }
+      };
+
+    const convertProductsToCsvData = (products) => { //se necesita esto para pasar los productos a una tabla
+      const headers = ['title', 'price', 'category']; //son los campos que va a mostrar
+      const rows = products.map(p => [p.title, p.price, p.category]); //acá los mapea
+      return [headers, ...rows];
     };
 
     const triggerDownload = (url, filename) => {
-        //crear el hipervinculo
-        const link = document.createElement("a");
-        link.href = url;
+        const link = document.createElement("a"); //Crea el hipervínculo en el dom
+        link.href = url; //le pasamos lo que va a tener
         link.download = filename;
-        //Agregamos el anchor tag en el DOM
-        document.body.appendChild(link);
-        //Simulas el click en el elemento
-        link.click();
-        //Eliminar el elemento anchor
-        document.body.removeChild(link);
+        document.body.appendChild(link); //lo agregamos al dom
+        link.click(); //hay que hacerle click
+        document.body.removeChild(link);  //Eliminamos el hipervínculo
     };
+  
 
 //--------------------------Return----------------------------------
   return (
@@ -182,30 +194,37 @@ function App() {
       />
       </div>
 
-    <h3 className='font-bold'>Descargar productos filtrados</h3>
+     {/* ----------Esportación ----------- */}    
     <div className='text-sm '>
       <label className="px-4 mr-2 font-semibold">Seleccionar formato:</label>
       <select className="border border-gray-300 rounded px-3 py-1" 
-        onChange={(e) => setFormat(e.target.value)} value={format}>
-        <option value="json">JSON</option>
+        onChange={(e) => setFormat(e.target.value)} value={format}> {/* onChange para que vaya cambiando el formato de acuerdo a lo que se elija */}
+        <option value="json">JSON</option> {/* y el value son las opciones */}
+        <option value="csv">CSV</option>
       </select>
-      <button className='bg-transparent hover:bg-pink-300 text-pink-400 font-semibold hover:text-white py-2 px-4 border border-pink-300 hover:border-transparent rounded ml-10' onClick={handleExport}>Exportar archivo</button>
+      <button className='bg-transparent hover:bg-pink-300 text-pink-400 font-semibold hover:text-white py-2 px-4 border border-pink-300 hover:border-transparent rounded ml-10' 
+      onClick={handleExport}>Exportar archivo</button> {/* onClick para que cuando le hagan click haga la exportación */}
     </div>
 
       <ProductsList products={sortedProducts} />
-      
+
+      {/*------- Paginación --------*/}
       <div className='flex justify-between w-1/3 mx-auto mb-30'>
         <button className='border px-4 py-2 rounded disabled:opacity-50 disabled:cursor-not-allowed' disabled={page === 1} onClick={() => {setPage(page - 1);}}>
           Página anterior
         </button>
+        {/* Usamos el diabled para desabilitar el boton cuando la pag sea igual a 1. Cuando haga click, el setPage va a hacer que page reste 1*/}
         <h3>{page}</h3>
         <button className='border px-4 py-2 rounded disabled:opacity-50 disabled:cursor-not-allowed' disabled={products.length < 30} onClick={() => {setPage(page + 1);}}>
           Página siguiente
         </button>
+        {/* Acá es igual, se deshabilita cuanod la long de los productos sea menor a 30 y con cada click, page va a ir aumentando gracias al setPage */}
       </div>
 
+      
       <div className="fondo text-center p-10">
-        <button className='bg-transparent hover:bg-pink-300 text-pink-400 font-semibold hover:text-white py-2 px-4 border border-pink-300 hover:border-transparent rounded mb-20' onClick={() => setShow(!show)}>{show ? "Ocultar Estadísticas" : "Mostrar Estadísticas"}</button> 
+        <button className='bg-transparent hover:bg-pink-300 text-pink-400 font-semibold hover:text-white py-2 px-4 border border-pink-300 hover:border-transparent rounded mb-20' 
+        onClick={() => setShow(!show)}>{show ? "Ocultar Estadísticas" : "Mostrar Estadísticas"}</button> 
       </div>
 
       
--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -2,10 +2,9 @@ function CategoryFilter({ selectedCategory, setSelectedCategory, categories }) {
   return (
     <div className="text-center my-4">
       <label className="mr-2 font-semibold">Filtrar por categoría:</label>
-      <select
+      <select className="border border-gray-300 rounded px-3 py-1"
         value={selectedCategory}
         onChange={(e) => setSelectedCategory(e.target.value)}
-        className="border border-gray-300 rounded px-3 py-1"
       >
         <option value="all">Todas</option>
         {categories.map((cat) => (<option key={cat} value={cat}>{cat}</option>))}
